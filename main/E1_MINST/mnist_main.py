b707e2eed5c5cda0d2a21ef56b289088b1ad4200
zhangjian <zhangjian@fruitday.com>
2019-01-09 15:29:25 +0800

    完善整体项目结构。

---
+++ main/E1_MINST/mnist_main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     mnist_main
+   Description :
+   Author :       zhangjian
+   date：          2019/1/9
+-------------------------------------------------
+   Change Activity:
+                   2019/1/9: 创建文件
+-------------------------------------------------
+"""
+
+from main.E1_MINST.config import config
+
+
+input_data_folder = config.input_data_folder
+output_data_folder = config.output_data_folder
+
+
+def main():
+    # 1st step, no need to download the mnist files, already in the input folder
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
